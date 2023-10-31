769d92abd109d5a2af611f02ad3a23a3b6c0aa6b
vswapna3202 <vswapna3202@gmail.com>
2023-10-31 21:16:19 +0000

    Refactor: Added validation code and fixed consts naming

--- src/components/NumberOfBeings.test.tsx
+++ src/components/NumberOfBeings.test.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import NumberOfBeings from './NumberOfBeings';
 
 test('renders NumberOfBeings Component', () => {
-    
-    render(<NumberOfBeings numberOfBeings={'7900000000'} onChangeNumberOfBeings={function (value: string): void {
-        throw new Error('Function not implemented.');
-    } } />        
+    const mockOnChangeNumberOfBeings = jest.fn();
+
+    render(<NumberOfBeings numberOfBeings={'7900000000'} onChangeNumberOfBeings={mockOnChangeNumberOfBeings} />        
     );
    
-        
+    
     const numberOfBeingsLabel = screen.getByLabelText('Number of Beings:');
     expect(numberOfBeingsLabel).toBeInTheDocument();
+    mockOnChangeNumberOfBeings.mockReturnValue(['']);
 
     const numberOfBeingsElement = screen.getByLabelText('Number of Beings:');
     expect(numberOfBeingsElement.getAttribute('value')).toBe('7900000000');
+    mockOnChangeNumberOfBeings.mockReturnValue(['']);
+
+    fireEvent.change(numberOfBeingsElement,{target:{value: -1}});
+    const errorMessage = 'Number of beings should be a number between 1 and 1000000000';
+    expect(screen.getByText(errorMessage)).toBeInTheDocument();
 });    
       
--- src/components/PlanetName.test.tsx
+++ src/components/PlanetName.test.tsx
@@ -6,16 +6,16 @@ test('renders PlanetName Component', () => {
     const mockOnChangePlanetName = jest.fn();
     
     render(<PlanetName planetName={'earth'} onChangePlanetName={mockOnChangePlanetName} />);
-    const mockOnChangeSpeciesName = jest.fn();  
+      
     const planetNameLabel = screen.getByLabelText('Planet Name:');
     expect(planetNameLabel).toBeInTheDocument();
 
     const planetNameElement = screen.getByLabelText('Planet Name:');
     expect(planetNameElement.getAttribute('value')).toBe('earth');
-    mockOnChangeSpeciesName.mockReturnValue(['']);
+    mockOnChangePlanetName.mockReturnValue(['']);
     fireEvent.change(planetNameElement,{target:{value:'mars'}});
     expect(mockOnChangePlanetName).toHaveBeenCalledWith('mars');
-    mockOnChangeSpeciesName.mockReturnValue(['']);
+    mockOnChangePlanetName.mockReturnValue(['']);
     fireEvent.change(planetNameElement,{target:{value:'%%%'}});
     const errorMessage = 'Planet name should be between 2 and 49 characters, and contain only letters';
     expect(screen.getByText(errorMessage)).toBeInTheDocument();
